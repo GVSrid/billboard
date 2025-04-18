f023fc45daca7b55e101630871ffdc9acb69daa7
GVSrid <152718647+GVSrid@users.noreply.github.com>
2025-04-18 09:17:09 +0530

    Add files via upload

---
+++ utils/piSignageService.js
@@ -0,0 +1,37 @@
+//import { post, get } from 'axios';
+import { USERNAME, PASSWORD, API_BASE } from '../config/credentials.js';
+import axios from 'axios';
+//const { USERNAME, PASSWORD, API_BASE } = require('../config/credentials');
+
+let token = null;
+
+async function login() {
+  const response = await post(`${API_BASE}/session`, {
+    email: USERNAME,
+    password: PASSWORD,
+    getToken: true,
+  });
+
+  if (response.data?.token) {
+    token = response.data.token;
+    return token;
+  } else {
+    throw new Error('Token not received');
+  }
+}
+
+async function getPlayers() {
+  if (!token) {
+    await login(); // auto login
+  }
+
+  const response = await get(`${API_BASE}/players`, {
+    headers: {
+      'x-access-token': token
+    }
+  });
+
+  return response.data;
+}
+
+export { login, getPlayers };
