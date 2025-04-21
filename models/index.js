a4cf3af26d6ba0de2f09769d1b6bb0bf4fbace82
GVSrid <152718647+GVSrid@users.noreply.github.com>
2025-04-21 14:48:57 +0530

    Delete models directory

--- models/index.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../config/database');
-
-const Booking = require('./booking')(sequelize, Sequelize.DataTypes);
-
-module.exports = {
-  sequelize,
-  Booking
-};
